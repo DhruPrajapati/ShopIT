1207b74c05bd0458b264ffb7e4e41f5db3df8bc3
Your Name <you@example.com>
2023-08-20 16:56:05 -0400

    implementing review route and its functionality

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,7 +6,11 @@ const {
     newProduct,
     getSingleProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    createProductReview,
+
+    getProductReviews,
+    deleteReview
 
 } = require("../controllers/productControllers");
 const { isAuthenticatedUser,authorizeRoles } = require("../middlewares/auth");
@@ -22,5 +26,11 @@ router.route("/admin/product/new").post(isAuthenticatedUser,authorizeRoles("admi
 router.route(`/admin/product/:id`)
     .put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct)
     .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct);
+
+router.route("/review").put(isAuthenticatedUser,createProductReview);
+router.route("/reviews")
+    .get(isAuthenticatedUser,getProductReviews )
+    .delete(isAuthenticatedUser,deleteReview )
+
  
 module.exports = router;  
